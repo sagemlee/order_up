2ff34b2d23aaf8531e569b6d7bc8a0a2f0f658bc
Sage Lee <sagemcklee@gmail.com>
2020-05-29 09:44:48 -0600

    Add calorie count spec

--- spec/features/dish_show_spec.rb
+++ spec/features/dish_show_spec.rb
@@ -1,20 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Dish Show Page" do
-  it "Displays list of ingredients for that dish" do
-     chef1 = Chef.create(name:"Bobbly Flay")
-     cheese = Ingredient.create(name:"cheese", calories: 120)
-     sauce = Ingredient.create(name:"sauce", calories: 40)
-     dough = Ingredient.create(name:"dough", calories: 150)
-     pizza = Dish.create(name:"pizza", description:"Yummy in my tummy", chef_id: chef1.id)
-     DishIngredient.create(dish_id: pizza.id, ingredient_id: cheese.id)
-     DishIngredient.create(dish_id: pizza.id, ingredient_id: sauce.id)
-     DishIngredient.create(dish_id: pizza.id, ingredient_id: dough.id)
+  before(:each) do
+  chef1 = Chef.create(name:"Bobbly Flay")
+  cheese = Ingredient.create(name:"cheese", calories: 120)
+  sauce = Ingredient.create(name:"sauce", calories: 40)
+  dough = Ingredient.create(name:"dough", calories: 150)
+  pizza = Dish.create(name:"pizza", description:"Yummy in my tummy", chef_id: chef1.id)
+  DishIngredient.create(dish_id: pizza.id, ingredient_id: cheese.id)
+  DishIngredient.create(dish_id: pizza.id, ingredient_id: sauce.id)
+  DishIngredient.create(dish_id: pizza.id, ingredient_id: dough.id)
+  visit "/dish/#{pizza.id}"
+  end
 
-     visit "/dish/#{pizza.id}"
-     
+  it "Displays list of ingredients for that dish" do
+
      expect(page).to have_content(cheese.name)
      expect(page).to have_content(sauce.name)
      expect(page).to have_content(dough.name)
+  end
+
+  it "Displays total calorie count of the dish" do
+    within ("#calorie") do
+      expect(page).to have_content(310)
+    end
   end
 end
