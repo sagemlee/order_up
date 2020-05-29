8a07295d366a1cecef956f49225f3dd9d2b1230b
Sage Lee <sagemcklee@gmail.com>
2020-05-29 09:20:16 -0600

    Add ingredient and dish_ingredient model specs

---
+++ spec/models/dish_ingredient_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe DishIngredient, type: :model do
+
+  describe "relationships" do
+    it {should belong_to :dishes}
+    it {should belong_to :ingredients}
+  end
+end
