baed1c3d23489daf4c6f314ece08a35c73555ff5
Sage Lee <sagemcklee@gmail.com>
2020-05-29 10:12:41 -0600

    Add unique inredients method to chef model spec

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe "methods" do
+    it "unique_ingredients"
+    @chef1 = Chef.create(name:"Bobbly Flay")
+    @cheese = Ingredient.create(name:"cheese", calories: 120)
+    @sauce = Ingredient.create(name:"sauce", calories: 40)
+    @dough = Ingredient.create(name:"dough", calories: 150)
+    @pizza = Dish.create(name:"pizza", description:"Yummy in my tummy", chef_id: @chef1.id)
+    @pasta = Dish.create(name:"pasta", description:"gooood", chef_id: @chef1.id)
+    @ingredient1 = DishIngredient.create(dish_id: @pizza.id, ingredient_id: @cheese.id)
+    @ingredient2 = DishIngredient.create(dish_id: @pizza.id, ingredient_id: @sauce.id)
+    @ingredient3 = DishIngredient.create(dish_id: @pizza.id, ingredient_id: @dough.id)
+    @ingredient1 = DishIngredient.create(dish_id: @pasta.id, ingredient_id: @cheese.id)
+    @ingredient2 = DishIngredient.create(dish_id: @pasta.id, ingredient_id: @sauce.id)
+    @ingredient3 = DishIngredient.create(dish_id: @pasta.id, ingredient_id: @dough.id)
+
+    expect(chef1.unique_ingredients).to eq([@ingredient1, @ingredient2, @ingredient3])
+  end
 end
